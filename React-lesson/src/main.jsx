9121caab8e56e30bf1b9c97e1e1d1b404e7f4520
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-03-25 12:18:08 +0100

    Add 3 task in React

--- React-lesson/src/main.jsx
+++ React-lesson/src/main.jsx
@@ -1,10 +1,68 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Task 1
+
+// const currentDate = new Date();
+// const year = currentDate.getFullYear();
+// const timer = <div>Mamy rok: {year}</div>;
+
+// Task 2
+// const currentDateTwo = new Date();
+// const hours = currentDateTwo.getHours();
+// const minutes = currentDateTwo.getMinutes();
+
+// const formatHours = hours < 10 ? `0${hours}` : hours;
+// const formatMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+// const timerTwo = (
+//   <div>
+//     {formatHours}:{formatMinutes}
+//   </div>
+// );
+
+// Task 3
+const days = [
+  'Niedziela',
+  'Poniedziałek',
+  'Wtorek',
+  'Środa',
+  'Czwartek',
+  'Piątek',
+  'Sobota',
+];
+const months = [
+  'stycznia',
+  'lutego',
+  'marca',
+  'kwietnia',
+  'maja',
+  'czerwca',
+  'lipca',
+  'sierpnia',
+  'września',
+  'października',
+  'listopada',
+  'grudnia',
+];
+
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = months[currentDate.getMonth()];
+const day = currentDate.getDate();
+const daysList = days[currentDate.getMonth()];
+const hours = currentDate.getHours();
+const minutes = currentDate.getMinutes();
+
+const formatHours = hours < 10 ? `0${hours}` : hours;
+const formatMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+const timerThree = (
+  <div>
+    {daysList}, {day}. {month}. {year}, godzina: {formatHours} :{' '}
+    {formatMinutes}
+  </div>
+);
+
+createRoot(document.getElementById('root')).render(timerThree);
