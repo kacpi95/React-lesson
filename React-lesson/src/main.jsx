d8e56994d4c3a3781b2c0ce2f4db3184ed84db4b
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-03-26 13:13:45 +0100

    Add 4 task in React

--- React-lesson/src/main.jsx
+++ React-lesson/src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react';
+import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
@@ -23,46 +24,56 @@ import App from './App.jsx';
 // );
 
 // Task 3
-const days = [
-  'Niedziela',
-  'Poniedziałek',
-  'Wtorek',
-  'Środa',
-  'Czwartek',
-  'Piątek',
-  'Sobota',
-];
-const months = [
-  'stycznia',
-  'lutego',
-  'marca',
-  'kwietnia',
-  'maja',
-  'czerwca',
-  'lipca',
-  'sierpnia',
-  'września',
-  'października',
-  'listopada',
-  'grudnia',
-];
+// const days = [
+//   'Niedziela',
+//   'Poniedziałek',
+//   'Wtorek',
+//   'Środa',
+//   'Czwartek',
+//   'Piątek',
+//   'Sobota',
+// ];
+// const months = [
+//   'stycznia',
+//   'lutego',
+//   'marca',
+//   'kwietnia',
+//   'maja',
+//   'czerwca',
+//   'lipca',
+//   'sierpnia',
+//   'września',
+//   'października',
+//   'listopada',
+//   'grudnia',
+// ];
 
-const currentDate = new Date();
-const year = currentDate.getFullYear();
-const month = months[currentDate.getMonth()];
-const day = currentDate.getDate();
-const daysList = days[currentDate.getMonth()];
-const hours = currentDate.getHours();
-const minutes = currentDate.getMinutes();
+// const currentDate = new Date();
+// const year = currentDate.getFullYear();
+// const month = months[currentDate.getMonth()];
+// const day = currentDate.getDate();
+// const daysList = days[currentDate.getMonth()];
+// const hours = currentDate.getHours();
+// const minutes = currentDate.getMinutes();
 
-const formatHours = hours < 10 ? `0${hours}` : hours;
-const formatMinutes = minutes < 10 ? `0${minutes}` : minutes;
+// const formatHours = hours < 10 ? `0${hours}` : hours;
+// const formatMinutes = minutes < 10 ? `0${minutes}` : minutes;
 
-const timerThree = (
-  <div>
-    {daysList}, {day}. {month}. {year}, godzina: {formatHours} :{' '}
-    {formatMinutes}
-  </div>
-);
+// const timerThree = (
+//   <div>
+//     {daysList}, {day}. {month}. {year}, godzina: {formatHours} :{' '}
+//     {formatMinutes}
+//   </div>
+// );
 
-createRoot(document.getElementById('root')).render(timerThree);
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Licznkik: {count}</h1>
+      <button onClick={() => setCount(count + 1)}></button>
+      <button onClick={() => setCount(count - 1)}></button>
+    </div>
+  );
+}
+createRoot(document.getElementById('root')).render(<Counter/>);
