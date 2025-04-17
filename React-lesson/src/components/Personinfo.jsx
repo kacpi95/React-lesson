50dd042cb678730538243e6fa6e4fe5e9f5cd917
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-04-17 13:09:29 +0200

    Add task

--- React-lesson/src/components/Personinfo.jsx
+++ React-lesson/src/components/Personinfo.jsx
@@ -4,6 +4,7 @@ export function Personinfo({ person }) {
       <h2>{person.name}</h2>
       <h3>{person.isAdmin == true ? 'Administrator' : 'Użytkownik'}</h3>
       <span>{person.age >= 18 ? 'Pełnoletni' : ''}</span>
+      {person.country == undefined ? '' : `Kraj pochodzenia: ${person.country}`}
       <hr />
     </>
   );
