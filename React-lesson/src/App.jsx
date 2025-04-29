2f62d2c41cfdbe083a8603bd2625b6c025f51ee6
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-04-29 12:17:18 +0200

    Add task

--- React-lesson/src/App.jsx
+++ React-lesson/src/App.jsx
@@ -1,59 +1,41 @@
-// import { useState } from 'react';
-
-// function App() {
-//   const [isSpoilerShown, setIsSpoilerShown] = useState(false);
-//   const [isWarningShown, setIsWarningShwon] = useState(true);
-
-//   function handleShowSpoilerCLick() {
-//     setIsSpoilerShown(true);
-//     setIsWarningShwon(false);
-//   }
-//   function handleCloseWarningClick() {
-//     setIsWarningShwon(false);
-//   }
-//   return (
-//     <>
-//       <h1>Gwiezdne Wojny V</h1>
-//       <h2>Rok produkcji: 1998</h2>
-//       <h2>Fabuła</h2>
-//       {isWarningShown && (
-//         <p>
-//           Uwaga! Opis fabuły zawiera spoilery!
-//           <button onClick={handleCloseWarningClick}>X</button>
-//         </p>
-//       )}
-//       <p>Dobrzy walczą ze złymi. Trzeba wyłączyć pole siłowe.</p>
-//       {isSpoilerShown ? (
-//         <p>Valder okazuje sie być ojcem Luka.</p>
-//       ) : (
-//         <button onClick={handleShowSpoilerCLick}>Pokaż spoiler</button>
-//       )}
-//     </>
-//   );
-// }
-
-// export default App;
 import { useState } from 'react';
 
-export default function App() {
-  const [isDark, setIsDark] = useState(false);
+function App() {
+  const [isSpoilerShown, setIsSpoilerShown] = useState(false);
+  const [isWarningShown, setIsWarningShwon] = useState(true);
+  const [numberOfLikes, setNumberOfLikes] = useState(0);
 
-  const toggleTheme = () => {
-    setIsDark(!isDark);
-  };
-
+  function handleShowSpoilerCLick() {
+    setIsSpoilerShown(true);
+    setIsWarningShwon(false);
+  }
+  function handleCloseWarningClick() {
+    setIsWarningShwon(false);
+  }
   return (
-    <div
-      style={{
-        backgroundColor: isDark ? '#333' : '#fff',
-        color: isDark ? '#fff' : '#000',
-        minHeight: '100vh',
-        padding: '20px',
-      }}
-    >
-      <button onClick={toggleTheme}>
-        Zmień motyw na {isDark ? 'jasny' : 'ciemny'}
+    <>
+      <h1>Gwiezdne Wojny V</h1>
+      <h2>Rok produkcji: 1998</h2>
+      <h2>Liczba polubień: {numberOfLikes}</h2>
+      <button onClick={() => setNumberOfLikes(numberOfLikes + 1)}>
+        Lubię to!
       </button>
-    </div>
+      <button onClick={() => setNumberOfLikes(numberOfLikes + 3)}>Kocham to!</button>
+      <h2>Fabuła</h2>
+      {isWarningShown && (
+        <p>
+          Uwaga! Opis fabuły zawiera spoilery!
+          <button onClick={handleCloseWarningClick}>X</button>
+        </p>
+      )}
+      <p>Dobrzy walczą ze złymi. Trzeba wyłączyć pole siłowe.</p>
+      {isSpoilerShown ? (
+        <p>Valder okazuje sie być ojcem Luka.</p>
+      ) : (
+        <button onClick={handleShowSpoilerCLick}>Pokaż spoiler</button>
+      )}
+    </>
   );
 }
+
+export default App;
