1db5745d8974fba7cba5afbbae3394c0a5618dc6
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-04-30 12:55:26 +0200

    Add task

--- React-lesson/src/App.jsx
+++ React-lesson/src/App.jsx
@@ -1,16 +1,61 @@
-import { LikesCounter } from './components/LikesCounter';
-import { Plot } from './components/Plot';
-import { Form } from './components/Form';
-function App() {
+// import { LikesCounter } from './components/LikesCounter';
+// import { Plot } from './components/Plot';
+// import { Form } from './components/Form';
+// function App() {
+//   return (
+//     <>
+//       <h1>Gwiezdne Wojny V</h1>
+//       <h2>Rok produkcji: 1998</h2>
+//       <LikesCounter />
+//       <Plot />
+//       <Form />
+//     </>
+//   );
+// }
+
+// export default App;
+import { useState } from 'react';
+
+export default function App() {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const [person, setPerson] = useState(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setPerson({ name, lastName });
+    setName('');
+    setLastName('');
+  }
+
   return (
     <>
-      <h1>Gwiezdne Wojny V</h1>
-      <h2>Rok produkcji: 1998</h2>
-      <LikesCounter />
-      <Plot />
-      <Form />
+      <h1>Dane osobowe:</h1>
+      {person && (
+        <>
+          <h2>
+            {person.name} {person.lastName}
+          </h2>
+        </>
+      )}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            placeholder='Imię'
+          />
+        </div>
+        <div>
+          <input
+            onChange={(e) => setLastName(e.target.value)}
+            value={lastName}
+            placeholder='Nazwisko'
+          />
+        </div>
+        <button>Zapisz</button>
+      </form>
     </>
   );
 }
-
-export default App;
