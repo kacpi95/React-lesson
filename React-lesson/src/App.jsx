4db689229900d2bd7f3ed88da3194842e02f0039
Kacper <kacpergrzywacz2@gmail.com>
2025-05-04 22:11:40 +0200

    Add task

--- React-lesson/src/App.jsx
+++ React-lesson/src/App.jsx
@@ -1,16 +1,39 @@
-import { LikesCounter } from './components/LikesCounter';
-import { Plot } from './components/Plot';
-import { Form } from './components/Form';
+// import { LikesCounter } from './components/LikesCounter';
+// import { Plot } from './components/Plot';
+// import { Form } from './components/Form';
+// function App() {
+//   return (
+//     <>
+//       <h1>Gwiezdne Wojny V</h1>
+//       <h2>Rok produkcji: 1998</h2>
+//       <LikesCounter />
+//       <Plot />
+//       <Form />
+//     </>
+//   );
+// }
+
+// export default App;
+
+import { useState } from "react";
+
 function App() {
-  return (
-    <>
-      <h1>Gwiezdne Wojny V</h1>
-      <h2>Rok produkcji: 1998</h2>
-      <LikesCounter />
-      <Plot />
-      <Form />
-    </>
-  );
+	const [isVisible, setIsVisible] = useState(true);
+
+	function handleClick() {
+		setIsVisible(!isVisible);
+	}
+	return (
+		<>
+			{isVisible && (
+				<h1>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
+					consequatur.
+				</h1>
+			)}
+			<button onClick={handleClick}>{isVisible ? "Ukryj" : "Pokaż"}</button>
+		</>
+	);
 }
 
 export default App;
