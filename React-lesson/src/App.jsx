241b98a2947ea7d50126c82cd1ea99070fbe03a3
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-05-12 21:25:32 +0200

    Add task

--- React-lesson/src/App.jsx
+++ React-lesson/src/App.jsx
@@ -1,16 +1,45 @@
-import { LikesCounter } from './components/LikesCounter';
-import { Plot } from './components/Plot';
-import { Form } from './components/Form';
+// import { LikesCounter } from './components/LikesCounter';
+// import { Plot } from './components/Plot';
+// import { Form } from './components/Form';
+// function App() {
+//   return (
+//     <>
+//       <h1>Gwiezdne Wojny V</h1>
+//       <h2>Rok produkcji: 1998</h2>
+//       <LikesCounter />
+//       <Plot />
+//       <Form />
+//     </>
+//   );
+// }
+
+// export default App;
+
+import { useState } from 'react';
 function App() {
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = () => {
+    if (!email.includes('@')) {
+      setError('Wpisz poprawny e-mail!');
+    } else {
+      setError('');
+      alert(`Wysłano: ${email}`);
+    }
+  };
+
   return (
-    <>
-      <h1>Gwiezdne Wojny V</h1>
-      <h2>Rok produkcji: 1998</h2>
-      <LikesCounter />
-      <Plot />
-      <Form />
-    </>
+    <div>
+      <input
+        type='email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder='Twój e-mail'
+      />
+      <button onClick={handleSubmit}>Wyślij</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </div>
   );
 }
-
 export default App;
